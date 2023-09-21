a08e8eb539782adbec8a40b56074390722d9d51f
farhnm <farhanm1146@gmail.com>
2023-09-21 22:28:37 +0700

    menambahkan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function  index()
+    {
+        echo "<h1>Perkenalkan<?h1><br>";
+        echo"Nama Saya = Farhan Maulana<br>
+            Saya tinggal di Lebak kantin<br>
+            olah raga yang saya sukai adalah
+            Panjat Tebing";
+    }
+}
